0fe630fc9e44304c715e073f0947cf1f9de64bfe
Lakshan Attanayake <lakshansandeep7@gmail.com>
2023-02-28 17:46:47 +0530

    added assets

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
+
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = 10
+let secondCard = 3
+
+let sum = firstCard + secondCard +10
+
+if (sum < 21) {
+    console.log("Do you want to add another card😀")
+} else if(sum === 21) {
+    console.log("Wohooo! you win BLACKJACK!!! 🥳")
+} else {
+    console.log("You're out of the game 😥")
+}
